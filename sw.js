a7d02032ffaa220e1be75ebe2bef53d63b88071a
flexxvfx <bearwallbear1@gmail.com>
2026-02-20 17:47:31 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-sw.js
+self.addEventListener('push', event => {
+    const data = event.data ? event.data.json() : { title:'Booking', body:'New booking placed!' };
+    const options = { body: data.body, icon:'images/paste.png', badge:'images/spray.png' };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
